6e92127377ca89b3e059872d048315e26fd54157
Anup Dhakal <dhakalanup015@gmail.com>
2026-01-11 19:59:45 +0545

    Data Ingestion Pipeline Completed

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -9,6 +9,4 @@ TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 # Data Ingestion Constants
 ARTIFACT_DIR: str = "artifacts"
 
-BUCKET_NAME: str = "lungxray24"
-
-S3_DATA_FOLDER: str = "data"
+BUCKET_NAME: str = "lungxray26"
